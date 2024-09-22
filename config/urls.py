70073f4acef5fa3ef1f61865902d68e3deef5adb
георгий савинцев <igsgo4645@gmail.com>
2024-09-22 18:40:07 +0400

    fix

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("mailing/", include("mail.urls", namespace="mailing"))
+    path("", include("mail.urls", namespace="mailing"))
 ]
