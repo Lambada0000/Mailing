e33b39b783870f8cde747e8a9ab01024114f8d76
георгий савинцев <igsgo4645@gmail.com>
2024-09-22 18:51:15 +0400

    add MEDIA_URL

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("mail.urls", namespace="mailing"))
-]
+    path("", include(("mail.urls", "mail"), namespace="mail"))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
