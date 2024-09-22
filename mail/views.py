13f207dc237c0ac1478d0e9e8473509ca7d45be0
георгий савинцев <igsgo4645@gmail.com>
2024-09-22 19:12:13 +0400

    add CRUD

--- mail/views.py
+++ mail/views.py
@@ -1,5 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from mail.models import Newsletter, Message, Client
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+class NewsletterListView(ListView):
+    model = Newsletter
+
+
+class NewsletterDetailView(DetailView):
+    model = Newsletter
+
+
+class NewsletterCreateView(CreateView):
+    model = Newsletter
+    fields = '__all__'
+    success_url = reverse_lazy('mail:newsletter_list')
+
+
+class NewsletterUpdateView(UpdateView):
+    model = Newsletter
+    fields = '__all__'
+    success_url = reverse_lazy('mail:newsletter_list')
+
+
+class NewsletterDeleteView(DeleteView):
+    model = Newsletter
+    success_url = reverse_lazy('mail:newsletter_list')
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    fields = '__all__'
+    success_url = reverse_lazy('mail:newsletter_list')
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageDetailView(DetailView):
+    model = Message
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    fields = '__all__'
+    success_url = reverse_lazy('mail:message_list')
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy('mail:message_list')
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = '__all__'
+    success_url = reverse_lazy('mail:client_list')
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = '__all__'
+    success_url = reverse_lazy('mail:client_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('mail:client_list')
